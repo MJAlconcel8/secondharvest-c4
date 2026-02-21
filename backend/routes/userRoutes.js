a4b98058622dd49cf8256bd473a39c595727b7ec
Mark Joseph Alconcel <109438699+MJAlconcel8@users.noreply.github.com>
2026-02-21 16:50:57 -0500

    fixed user route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { registerUser, loginUser } = require('../controllers/userController');
+const { registerUser, loginUser, getUserById, updateUser, deleteUser} = require('../controllers/userController');
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-
+router.delete('/:id', deleteUser);
+router.put('/:id', updateUser);
+router.get('/:id', getUserById);
 module.exports = router;

6f6003e67c5199a9b5e7705baad01a69e0614151
Mark Joseph Alconcel <109438699+MJAlconcel8@users.noreply.github.com>
2026-02-21 16:46:34 -0500

    made a .env (can't dox personal info)

--- backend/config/userConfig.js
+++ backend/config/userConfig.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'alconcel',
-    host: 'localhost',
-    port: 5432,
-    database: 'capstone'
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
 });
 
 pool.on('error', (err) => {
